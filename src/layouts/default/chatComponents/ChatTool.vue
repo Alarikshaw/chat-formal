d75c9b29d9a09f1b9f1ea40f47c44aaf66bd9432
alarikshaw <337349132@qq.com>
2021-01-11 16:21:28 +0800

    bg => 修复

--- src/layouts/default/chatComponents/ChatTool.vue
+++ src/layouts/default/chatComponents/ChatTool.vue
@@ -254,7 +254,10 @@ export default defineComponent({
         state.uploading = false;
         state.showUpload = false;
         createMessage.success(data.data.msg);
-        userStore.getUserInfoAction(data.data.data);
+        console.log('data.data.data', data.data.data);
+        let param = data.data.data;
+        param.background = userStore.getUserInfoState().background;
+        userStore.getUserInfoAction(param);
         state.avatar = data.data.data.avatar;
         // 通知其他用户个人信息改变
       } else {
