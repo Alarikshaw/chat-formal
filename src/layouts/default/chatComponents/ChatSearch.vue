8dd3d7edf7f10c9ffde8d8950a30f35bc2ac1fa5
alarikshaw <337349132@qq.com>
2021-01-11 17:41:11 +0800

    select error

--- src/layouts/default/chatComponents/ChatSearch.vue
+++ src/layouts/default/chatComponents/ChatSearch.vue
@@ -1,35 +1,91 @@
 <template>
-    <div class="search">
-        <div class="search-select">
-            <Select
-                show-search
-                placeholder="搜索聊天组"
-                default-active-first-option="false"
-                :show-arrow="false"
-                :filter-option="false"
-                :not-found-content="null"
-                @search="handleSearch"
-                >
-            </Select>
-        </div>
+  <div class="search">
+    <div class="search-select">
+      <Select
+        show-search
+        placeholder="搜索聊天组"
+        :show-arrow="false"
+        :filter-option="false"
+        :not-found-content="null"
+        @search="handleSearch"
+      >
+
+      </Select>
+      <Dropdown
+        class="search-dropdown"
+        placement="bottomCenter"
+        :trigger="['click']"
+      >
+        <PlusCircleOutlined />
+        <template #overlay>
+          <Menu
+            selectable
+            v-model:selectedKeys="state.visibleAddGroup"
+          >
+            <MenuItem key="default"><span>创建群</span></MenuItem>
+            <MenuItem key="middle"><span>搜索群</span></MenuItem>
+            <MenuItem key="small"><span>搜索用户</span></MenuItem>
+          </Menu>
+        </template>
+      </Dropdown>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue, { defineComponent } from 'vue';
-import { Select } from 'ant-design-vue';
+import { defineComponent, reactive } from 'vue';
+import { Select, Dropdown, Menu } from 'ant-design-vue';
+import { PlusCircleOutlined } from '@ant-design/icons-vue';
 export default defineComponent({
-    name: 'ChatSearch',
-    components: {
-        Select
-    },
-    setup() {
-        function handleSearch() {
-
-        }
-        return {
-            handleSearch
-        }
-    }
-})
+  name: 'ChatSearch',
+  components: {
+    Select,
+    Dropdown,
+    MenuItem: Menu.Item,
+    PlusCircleOutlined,
+  },
+  setup() {
+    const origin: any = {
+      visibleAddGroup: true,
+    };
+    const state = reactive(origin);
+    function handleSearch() {}
+    return {
+      handleSearch,
+      state,
+    };
+  },
+});
 </script>
+
+<style lang="less" scoped>
+.search {
+  position: relative;
+  //   height: 60px;
+  //   padding: 10px;
+  display: flex;
+  align-items: center;
+  .search-select {
+    width: 100%;
+    .ant-select {
+      width: 100%;
+    }
+  }
+  .search-dropdown {
+    position: absolute;
+    right: 10px;
+    top: 13px;
+    width: 40px;
+    height: 34px;
+    font-size: 20px;
+    cursor: pointer;
+    line-height: 40px;
+    color: gray;
+    transition: 0.2s all linear;
+    border-radius: 4px;
+    &:hover {
+      background-color: skyblue;
+    }
+  }
+}
+</style>
